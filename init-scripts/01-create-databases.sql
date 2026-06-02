834c3768aece3e45e5992d2ab1f8142ebd8a0329
DarkoRodriguez <dark.rodriguez@duocuc.cl>
2026-06-02 09:38:15 -0400

    hotfix: conectar bases de datos

--- init-scripts/01-create-databases.sql
+++ init-scripts/01-create-databases.sql
@@ -3,7 +3,17 @@ CREATE DATABASE ms_usuarios;
 CREATE DATABASE ms_mapeo;
 CREATE DATABASE demo_db;
 
+-- Conectar a cada base para inicializarla
+\connect ms_usuarios
+
+\connect ms_mapeo
+CREATE EXTENSION IF NOT EXISTS postgis;
+\connect demo_db
+
+\connect postgres
+
 -- Asignar permisos
 GRANT ALL PRIVILEGES ON DATABASE ms_usuarios TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE ms_mapeo TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE demo_db TO postgres;
+
